0c3258dc2e17a0c4ac9f75e97e43f0fc3cf18b6b
Doudou210 <siakakoudou.doumbia@yahoo.com>
2025-02-16 09:49:51 +0100

    refactory of header

--- frontend/src/components/navBars/Header.tsx
+++ frontend/src/components/navBars/Header.tsx
@@ -1,7 +1,5 @@
 import { Link, useNavigate } from "react-router-dom";
 import "../../assets/css/navBar/NavBar.css"
-import "bootstrap/dist/css/bootstrap.min.css";
-
 
 export default function Header() {
   const navigate = useNavigate();
@@ -13,12 +11,12 @@ export default function Header() {
   }
   return (
     <nav className="navbar navbar-expand-lg bg-primary-subtle p-4">
-      <div className="container-fluid d-flex justify-content-evenly align-items-center">
+      <div className="container-fluid d-flex justify-content-between align-items-center">
         <h3><Link to={"/"}> AdobStud </Link></h3>
         <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
           <span className="navbar-toggler-icon"></span>
         </button>
-        <div className="collapse navbar-collapse" id="navbarSupportedContent">
+        <div className="collapse navbar-collapse justify-content-center" id="navbarSupportedContent">
           <ul className="navbar-nav mx-auto mb-2 mb-lg-0">
             <li className="nav-item">
               <Link to={"/"} className="nav-link active" aria-current="page">Accueil</Link>
@@ -29,8 +27,12 @@ export default function Header() {
             <li className="nav-item">
               <Link to={"/mes_favoris"} className="nav-link"> Mes Favoris</Link>
             </li>
+            <hr />
           </ul>
-          <button className="btn btn-outline-success" type="button" onClick={handleClickAuth}>Se connecter</button>
+          <div className="d-flex justify-content-center align-items-center">
+            <button className="btn btn-outline-primary w-50" type="button" onClick={handleClickAuth}>Se connecter ou s'inscrire</button><br />
+            <button className="btn btn-outline-danger w-50" type="button" onClick={handleClickAuth}>Se Déconnecté(e)</button>
+          </div>
         </div>
       </div>
     </nav>
--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,9 +1,11 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import Home from './components/Home.tsx'
-import { createBrowserRouter, RouterProvider } from 'react-router-dom'
-import AuthUser from './components/authUsers/AuthUser.tsx'
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+import './index.css';
+import Home from './components/Home.tsx';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import AuthUser from './components/authUsers/AuthUser.tsx';
+import "bootstrap/dist/css/bootstrap.min.css";
+import "bootstrap/dist/js/bootstrap.bundle.min.js";
 
 const router = createBrowserRouter([
   {
