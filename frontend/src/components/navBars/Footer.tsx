a0feb399ea1cb68ace7c9fbfc323db6d3bb2bbd1
Doudou210 <siakakoudou.doumbia@yahoo.com>
2025-02-17 11:19:48 +0100

    add footer

--- frontend/src/components/navBars/Footer.tsx
+++ frontend/src/components/navBars/Footer.tsx
@@ -1,9 +1,40 @@
+import { Facebook, Instagram, LinkedIn, WhatsApp } from '@mui/icons-material'
 import React from 'react'
 
 export default function Footer() {
   return (
-    <div>
-        <h1>Footer</h1>
+    <div className='bg-primary-subtle p-4'>
+      <div className='container-fluid d-flex justify-content-between align-items-center'>
+        <div>
+          <h1>Logo & resS </h1>
+          <ul className='d-flex p-4'>
+            <li>
+              <LinkedIn/>
+            </li>
+            <li><WhatsApp/></li>
+            <li><Instagram/></li>
+            <li><Facebook/></li>
+          </ul>
+        </div>
+        <div>
+          <h1>Emploi</h1>
+          <ul>
+            <li>Qui sommes-nous</li>
+            <li>Offres d'emploi</li>
+            <li>Offres de stage</li>
+            <li>Centre d'aide</li>
+          </ul>
+        </div>
+        <div>
+          <h1>NewsLetter</h1>
+          <div>
+            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit.</p>
+            <input type="email" id='email' placeholder='Email' />
+            <button type="submit">S'inscrire</button>
+            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit.</p>
+          </div>
+        </div>
+      </div>
     </div>
   )
 }
