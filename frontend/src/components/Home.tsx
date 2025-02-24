722d88aff4b546e74f26b9d738ea96eed7eb95fa
Doudou210 <siakakoudou.doumbia@yahoo.com>
2025-02-24 11:38:04 +0100

    refactory of style footer

--- frontend/src/components/Home.tsx
+++ frontend/src/components/Home.tsx
@@ -1,20 +1,15 @@
-import { useNavigate } from "react-router-dom"
+// import { useNavigate } from "react-router-dom"
 import "../assets/css/Home.css"
 import Footer from "./navBars/Footer";
 import Header from "./navBars/Header";
 
 export default function Home() {
-  const navigate= useNavigate();
-
-  const handleNav= ()=>{
-    navigate('/auth_user')
-  }
+  // const navigate= useNavigate();
 
   return (
     <div>
       <Header/>
       <h1 className="d-flex align-items-center justify-content-center vh-100">Home Page</h1>
-      <button type="button" onClick={handleNav}>Auth User</button>
       <Footer/>
     </div>
   )
--- frontend/src/components/navBars/Footer.tsx
+++ frontend/src/components/navBars/Footer.tsx
@@ -1,45 +1,55 @@
-import { Facebook, Instagram, LinkedIn, WhatsApp } from '@mui/icons-material'
+import { Facebook, Instagram, LinkedIn, WhatsApp } from '@mui/icons-material';
+import "../../assets/css/navBar/NavBar.css";
+import { Link } from 'react-router-dom';
 
 export default function Footer() {
   return (
-    <div className='navbar navbar-expand-lg bg-primary-subtle p-4'>
-      <div className='container-fluid d-flex justify-content-between align-items-center'>
-        <div>
-          <h1>Logo & resS </h1>
-          <ul className='d-flex p-4'>
-            <li>
-              <LinkedIn/>
-            </li>
-            <li>
-              <WhatsApp/>
-            </li>
-            <li>
-              <Instagram/>
-            </li>
-            <li>
-              <Facebook/>
-            </li>
-          </ul>
-        </div>
-        <div>
-          <h1>Emploi</h1>
-          <ul>
-            <li>Qui sommes-nous</li>
-            <li>Offres d'emploi</li>
-            <li>Offres de stage</li>
-            <li>Centre d'aide</li>
-          </ul>
-        </div>
-        <div>
-          <h1>NewsLetter</h1>
-          <div>
+    <div className='containerFooter bg-primary-subtle p-4'>
+      <div className='container '>
+        <div className='row text-center yexy-md-start'>
+          <div className='col-12 col-md-4 text-center text-md-start mb-4 mb-md-0'>
+            <h1>AdobStud </h1>
+            <ul className='d-flex justify-content-center justify-content-md-start list-unstyled gap-3'>
+              <li>
+                <LinkedIn fontSize='large'/>
+              </li>
+              <li>
+                <WhatsApp fontSize='large'/>
+              </li>
+              <li>
+                <Instagram fontSize='large'/>
+              </li>
+              <li>
+                <Facebook fontSize='large'/>
+              </li>
+            </ul>
+          </div>
+          <div className='col-12 col-md-4 text-center text-md-start mb-4 mb-md-0'>
+            <h1>Emploi</h1>
+            <ul className='list-unstyled gap-3'>
+              <li>Qui sommes-nous</li>
+              <li>Offres d'emploi</li>
+              <li>Offres de stage</li>
+              <li>Centre d'aide</li>
+            </ul>
+          </div>
+          <div className='col-12 col-md-4 text-center text-md-start mb-4 mb-md-0'>
+            <h1>NewsLetter</h1>
             <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit.</p>
-            <input type="email" id='email' placeholder='Email' />
-            <button type="submit">S'inscrire</button>
-            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit.</p>
+            <div className='d-flex justify-content-center align-items-center'>
+              <input type="email" id='email' placeholder='Email' />
+              <button type="submit">S'inscrire</button>
+            </div>
+            <div className='d-flex align-items-center justify-content-center'>
+              <p>
+                Vous pouvez vous désabonner à tout moment. On n'est pas susceptibles, promis. Pour en savoir plus sur notre politique de protection des données, 
+                <Link to={"/"}>cliquez-ici.</Link>
+              </p>
+            </div>
           </div>
         </div>
       </div>
+      <p className='bg-primary p-2 text-center mt-4 text-white'>Copy by DevCode</p>
     </div>
   )
 }
