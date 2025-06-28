844add863a169170fc6e1d6a0e81010c81a76cc3
Doudou210 <siakakoudou.doumbia@yahoo.com>
2025-06-28 21:33:59 +0200

    Refactor Home component: replace content with Header component and remove unused assets

--- frontend/src/app/aide/page.tsx
+++ frontend/src/app/aide/page.tsx
@@ -1,9 +1,9 @@
 import React from 'react'
 
-export default function Header() {
+export default function aide() {
   return (
     <div>
-      <h1>Header</h1>
+      <h1>Aide-moi</h1>
     </div>
   )
 }
